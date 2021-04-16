ea7a56d897f18c3f356e6f455b42107aae42a3b8
ru14 <74753345+ru14@users.noreply.github.com>
2021-04-16 14:13:53 -0700

    server conected

--- server.js
+++ server.js
@@ -2,25 +2,35 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 const util = require("util");
-const notes = require("./db/db.json");
+const notes = require("./Develop/db/db.json");
 const path = require("path")
 const publicDirectoryPath = path.join(__dirname, "./Develop/public");
 
 app.use(express.urlencoded({ extended: true})) ;
 app.use(express.json());
-app.use(express.static("publicDirectoryPath"));
+app.use(express.static(publicDirectoryPath));
 
-
+// join and save notes
 app.get('/api/notes', function (req, res) {
   res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
 });
 
+
+// new note
+const uuid = require("uuid");
 app.post("/api/notes", (req, res) => {
 const notes = Json.parse(fs.readFileSync("./Develop/db/db.json"));
 const newNotes = req.body;
+newNotes.id = uuid.v4();
 notes.push(newNotes);
 fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
 res.json(notes);
 });
  
+// delete specific notes
+app.delete("/api/notes/:id", (req, res) => {
+const notes = Json.parse(fs.readFileSync("./Develop/db/db.json"));
+const deletelNote = notes.filter((removeNote) => removeNote.id !== req.parse.id);
+res.json(deletelNote);
+});
 app.listen(3000)
