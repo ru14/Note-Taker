8004d2afc9d52bb652cbfa9c12274aabffade018
ru14 <74753345+ru14@users.noreply.github.com>
2021-04-17 13:02:29 -0700

    erroe fixed

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true})) ;
 app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
+
+
 // join and save notes
 app.get('/api/notes', function (req, res) {
   res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
@@ -17,20 +19,25 @@ app.get('/api/notes', function (req, res) {
 
 // new note
 const uuid = require("uuid");
+
 app.post("/api/notes", (req, res) => {
-const notes = Json.parse(fs.readFileSync("./Develop/db/db.json"));
+const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./Develop/db/db.json")));
 const newNotes = req.body;
+console.log({newNotes});
 newNotes.id = uuid.v4();
 notes.push(newNotes);
-fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
+fs.writeFileSync(path.join(__dirname, "./Develop/db/db.json"), JSON.stringify(notes));
 res.json(notes);
 });
  
 // delete specific notes
 app.delete("/api/notes/:id", (req, res) => {
-const notes = Json.parse(fs.readFileSync("./Develop/db/db.json"));
-const deletelNote = notes.filter((removeNote) => removeNote.id !== req.parse.id);
-res.json(deletelNote);
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "./Develop/db/db.json")));
+const deletelNote = notes.filter(removeNote => removeNote.id !== req.params.id);
+fs.writeFileSync(path.join(__dirname, "./Develop/db/db.json"), JSON.stringify(deletelNote));
+
+console.log({deletelNote})
+res.json(notes);
 });
 
 
@@ -39,7 +46,7 @@ app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
-app.get("/", function(req, res){
+app.get("/notes", function(req, res){
   res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
